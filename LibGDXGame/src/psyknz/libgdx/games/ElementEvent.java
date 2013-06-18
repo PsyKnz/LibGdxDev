66da4c7a4f6fdc7f0b92536f2be5254aa207b1dc
Alex Crowther <crowther.aj@gmail.com>
2013-06-18 17:47:10 +0800

    Added ElementEvent

---
+++ LibGDXGame/src/psyknz/libgdx/games/ElementEvent.java
@@ -0,0 +1,23 @@
+package psyknz.libgdx.games;
+
+public class ElementEvent {
+	
+	/* The following types of events can be generated:
+	 * - TOUCHED: Should be used when the user has interacted directly with the GameElement. 
+	 * - COLLIDED: Should be used when the GameElement has collided with some other object on the current GameScreen. 
+	 * - TIMER_FINISHED: Should be used when a timer has run down to 0. */
+	public static enum EventType {
+		TOUCHED, COLLIDED, TIMER_FINISHED
+	}
+	
+	// Reference to the element generating the event.
+	public GameElement element;
+	
+	// The type of event generated.
+	public EventType type;
+	
+	public ElementEvent(GameElement element, EventType type) {
+		this.element = element;
+		this.type = type;
+	}
+}
