9146276fe33177a4a01012631e6d7005c440f3e3
PsyKnz <Crowther.aj@gmail.com>
2013-08-17 10:50:14 +0800

    Begun work on Magnetris

---
+++ LibGDXGame/src/psyknz/libgdx/games/magnetris/PlayScreen.java
@@ -0,0 +1,10 @@
+package psyknz.libgdx.games.magnetris;
+
+import psyknz.libgdx.games.*;
+
+public class PlayScreen extends GameScreen {
+    
+	public PlayScreen(LibGDXGame game) {
+		super(game);
+	}
+}
